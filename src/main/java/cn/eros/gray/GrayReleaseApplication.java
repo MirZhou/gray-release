b8e822b2d607dc77d5bca9a1679f7daac221661a
Eros <mir3732@live.com>
2024-05-09 09:49:30 +0800

    first commit

---
+++ src/main/java/cn/eros/gray/GrayReleaseApplication.java
@@ -0,0 +1,13 @@
+package cn.eros.gray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrayReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrayReleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/eros/gray/GrayReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.eros.gray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrayReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
