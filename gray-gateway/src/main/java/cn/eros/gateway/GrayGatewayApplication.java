af3a7c8bf15d02d550d6bab91d3769c4262e1f1a
Eros <mir3732@live.com>
2024-05-09 14:44:39 +0800

    新增网关模块

---
+++ gray-gateway/src/main/java/cn/eros/gateway/GrayGatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.eros.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eros
+ * @since 2024/5/9 11:43
+ */
+@SpringBootApplication
+public class GrayGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrayGatewayApplication.class, args);
+    }
+}
